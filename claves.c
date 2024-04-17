33cde8f4e6fc35ac76221d6c893d206d389b0517
GLuis189 <189luis189@gmail.com>
2024-04-17 20:48:21 +0200

    dasf

--- claves.c
+++ claves.c
@@ -6,156 +6,160 @@
 
 
 int init(){
-	CLIENT *clnt;
-	int  *result_1;
+    CLIENT *clnt;
+    enum clnt_stat retval_1;
+    int result_1;
 
     char *host = getenv("IP_TUPLAS");
 
-	clnt = clnt_create (host, CLAVE_VALOR, CLAVE_VALOR_V1, "tcp");
-	if (clnt == NULL) {
-		clnt_pcreateerror (host);
-		exit (1);
-	}
+    clnt = clnt_create(host, CLAVE_VALOR, CLAVE_VALOR_V1, "tcp");
+    if (clnt == NULL) {
+        clnt_pcreateerror(host);
+        exit(1);
+    }
 
-    result_1 = init_1(clnt);
-	if (result_1 == (int *) NULL) {
-		clnt_perror (clnt, "call failed");
-	}
-    
-    clnt_destroy (clnt);
-    return *result_1;
+    retval_1 = init_1(&result_1, clnt);
+    if (retval_1 != RPC_SUCCESS) {
+        clnt_perror(clnt, "call failed");
+    }
+
+    return result_1;
 }
 
 int set_value(int clave, char *valor1, int N_value2, double *V_value2){
     CLIENT *clnt;
-    int  *result_2;
-    value_args  set_value_1_arg1;
+    enum clnt_stat retval_2;
+    int result_2;
+    value_args set_value_1_arg1;
 
     char *host = getenv("IP_TUPLAS");
 
-    clnt = clnt_create (host, CLAVE_VALOR, CLAVE_VALOR_V1, "tcp");
+    clnt = clnt_create(host, CLAVE_VALOR, CLAVE_VALOR_V1, "tcp");
     if (clnt == NULL) {
-        clnt_pcreateerror (host);
-        exit (1);
+        clnt_pcreateerror(host);
+        exit(1);
     }
 
     set_value_1_arg1.key = clave;
     set_value_1_arg1.value1 = valor1;
     set_value_1_arg1.N_value2 = N_value2;
     set_value_1_arg1.V_value2.double_array_len = N_value2;
-    set_value_1_arg1.V_value2.double_array_val = V_value2;
-
-    result_2 = set_value_1(set_value_1_arg1, clnt);
-    if (result_2 == (int *) NULL) {
-        clnt_perror (clnt, "call failed");
+    set_value_1_arg1.V_value2.double_array_val = (double *)malloc(N_value2 * sizeof(double));
+    for (int i = 0; i < N_value2; i++) {
+        set_value_1_arg1.V_value2.double_array_val[i] = V_value2[i];
     }
 
-    clnt_destroy (clnt);
-    return *result_2;
+    retval_2 = set_value_1(set_value_1_arg1, &result_2, clnt);
+    if (retval_2 != RPC_SUCCESS) {
+        clnt_perror(clnt, "call failed");
+    }
+
+    return result_2;
 }
 
 int get_value(int key, char *value1, int *N_value2, double *V_value2){
     CLIENT *clnt;
-    get_value_result  *result_3;
-    int get_value_1_arg1;
+    enum clnt_stat retval_3;
+    get_value_result result_3;
+    int get_value_1_key;
 
     char *host = getenv("IP_TUPLAS");
 
-    clnt = clnt_create (host, CLAVE_VALOR, CLAVE_VALOR_V1, "tcp");
+    clnt = clnt_create(host, CLAVE_VALOR, CLAVE_VALOR_V1, "tcp");
     if (clnt == NULL) {
-        clnt_pcreateerror (host);
-        exit (1);
+        clnt_pcreateerror(host);
+        exit(1);
     }
 
-    get_value_1_arg1 = key;
+    get_value_1_key = key;
 
-    result_3 = get_value_1(get_value_1_arg1, clnt);
-    if (result_3 == (value_args *) NULL) {
-        clnt_perror (clnt, "call failed");
+    retval_3 = get_value_1(get_value_1_key, &result_3, clnt);
+    if (retval_3 != RPC_SUCCESS) {
+        clnt_perror(clnt, "call failed");
     }
 
-    strcpy(value1, result_3->value1);
-    *N_value2 = result_3->N_value2;
-    for (int i = 0; i < *N_value2; i++){
-        V_value2[i] = result_3->V_value2.double_array_val[i];
+    if (result_3.status == 0) {
+        strcpy(value1, result_3.value1);
+        *N_value2 = result_3.N_value2;
+        for (int i = 0; i < result_3.N_value2; i++) {
+            V_value2[i] = result_3.V_value2.double_array_val[i];
+        }
     }
 
-    clnt_destroy (clnt);
-    return result_3->status;
+    return result_3.status;
 }
 
 int modify_value(int clave, char *valor1, int N_value2, double *V_value2){
     CLIENT *clnt;
-    int  *result_4;
-    value_args  modify_value_1_arg1;
+    enum clnt_stat retval_4;
+    int result_4;
+    value_args modify_value_1_arg1;
 
     char *host = getenv("IP_TUPLAS");
 
-    clnt = clnt_create (host, CLAVE_VALOR, CLAVE_VALOR_V1, "tcp");
+    clnt = clnt_create(host, CLAVE_VALOR, CLAVE_VALOR_V1, "tcp");
     if (clnt == NULL) {
-        clnt_pcreateerror (host);
-        exit (1);
+        clnt_pcreateerror(host);
+        exit(1);
     }
 
     modify_value_1_arg1.key = clave;
     modify_value_1_arg1.value1 = valor1;
     modify_value_1_arg1.N_value2 = N_value2;
     modify_value_1_arg1.V_value2.double_array_len = N_value2;
-    modify_value_1_arg1.V_value2.double_array_val = V_value2;
-
-    result_4 = modify_value_1(modify_value_1_arg1, clnt);
-    if (result_4 == (int *) NULL) {
-        clnt_perror (clnt, "call failed");
+    modify_value_1_arg1.V_value2.double_array_val = (double *)malloc(N_value2 * sizeof(double));
+    for (int i = 0; i < N_value2; i++) {
+        modify_value_1_arg1.V_value2.double_array_val[i] = V_value2[i];
     }
 
-    clnt_destroy (clnt);
-    return *result_4;
+    retval_4 = modify_value_1(modify_value_1_arg1, &result_4, clnt);
+    if (retval_4 != RPC_SUCCESS) {
+        clnt_perror(clnt, "call failed");
+    }
+
+    return result_4;
 }
 
 int delete_key(int clave){
     CLIENT *clnt;
-    int  *result_5;
-    int delete_key_1_arg1;
+    enum clnt_stat retval_5;
+    int result_5;
 
     char *host = getenv("IP_TUPLAS");
 
-    clnt = clnt_create (host, CLAVE_VALOR, CLAVE_VALOR_V1, "tcp");
+    clnt = clnt_create(host, CLAVE_VALOR, CLAVE_VALOR_V1, "tcp");
     if (clnt == NULL) {
-        clnt_pcreateerror (host);
-        exit (1);
+        clnt_pcreateerror(host);
+        exit(1);
     }
 
-    delete_key_1_arg1 = clave;
-
-    result_5 = delete_key_1(delete_key_1_arg1, clnt);
-    if (result_5 == (int *) NULL) {
-        clnt_perror (clnt, "call failed");
+    retval_5 = delete_key_1(clave, &result_5, clnt);
+    if (retval_5 != RPC_SUCCESS) {
+        clnt_perror(clnt, "call failed");
     }
 
-    clnt_destroy (clnt);
-    return *result_5;
+    return result_5;
 }
 
 int exist(int clave){
     CLIENT *clnt;
-    int  *result_6;
-    int exist_1_arg1;
+    enum clnt_stat retval_6;
+    int exist_1_key;
 
     char *host = getenv("IP_TUPLAS");
 
-    clnt = clnt_create (host, CLAVE_VALOR, CLAVE_VALOR_V1, "tcp");
+    clnt = clnt_create(host, CLAVE_VALOR, CLAVE_VALOR_V1, "tcp");
     if (clnt == NULL) {
-        clnt_pcreateerror (host);
-        exit (1);
+        clnt_pcreateerror(host);
+        exit(1);
     }
 
-    exist_1_arg1 = clave;
+    exist_1_key = clave;
 
-    result_6 = exist_1(exist_1_arg1, clnt);
-    if (result_6 == (int *) NULL) {
-        clnt_perror (clnt, "call failed");
+    retval_6 = exist_1(exist_1_key, &exist_1_key, clnt);
+    if (retval_6 != RPC_SUCCESS) {
+        clnt_perror(clnt, "call failed");
     }
 
-    clnt_destroy (clnt);
-    return *result_6;
+    return exist_1_key;
 }
